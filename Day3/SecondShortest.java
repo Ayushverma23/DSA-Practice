f874150d52c594a87cb78ac19c98cc0822ae8360
Ayushverma92 <ayushverma.ara19@gmail.com>
2025-07-24 22:23:18 +0530

    day3

---
+++ Day3/SecondShortest.java
@@ -0,0 +1,18 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class SecondShortest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array : ");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements of array: ");
+        int arr[] = new int[n];
+        for (int i =0 ; i<n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println("The second shortest element in array is : " + arr[1]);
+        sc.close();
+    }
+}
--- Day3/hi.java
+++
@@ -1,8 +0,0 @@
-public class hi {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        // This is a simple Java program that prints "Hello, World!" to the console.
-        // You can run this program to see the output.
-        System.out.println("Welcome to Day 3 of Java programming!");
-    }
-}
